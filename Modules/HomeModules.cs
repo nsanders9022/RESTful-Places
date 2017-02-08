6b59cc9349e3b09e76d0f0e86c3f69f2a9660548
Caitlin Hines and Nicole Sanders <caitlin+nicole@>
2017-02-08 10:00:22 -0800

    add initial files/folders, add class Place in Tasks.cs, and fill in the HomeModule page

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,33 @@
+using Nancy;
+using Place.Objects;
+using System.Collections.Generic;
+
+namespace Places
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get['/'] = _ => {
+        return View['index.cshtml'];
+      };
+      Get['/places'] = _ =>
+      {
+        List<Place> allPlaces = Place.GetAll();
+        return View['place.cshtml'];
+      };
+      Get['/places/new'] = _ => {
+        return View['place_form.cshtml'];
+      };
+      Get['/places/{id}'] = parameters => {
+        Place place = Place.Find(parameters.id);
+        return View['place.cshtml', place];
+      };
+      Post['/places'] = _ => {
+        Place newPlace = new Place(Request.Form['new-place']);
+        List<Place> allPlaces = Place.GetAll();
+        return View["places.cshtml", allPlaces;
+      };
+    }
+  }
+}
